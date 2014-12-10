067d834d1086b9457677ab639900d720d43bac1d
Erystasius <Felix.E.Huang@gmail.com>
2014-12-10 11:38:40 +0800

    delete example

--- view/v_viewitem.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('../smarty/libs/Smarty.class.php');
-
-	GLOBAL $items;
-
-	$viewitem = new smarty();
-
-	$viewitem->assign("items",$items);
-	$viewitem->display("template/viewitem.tpl");
