1d4a81b418a0cb5ec68cbb5ccbff61ef97a7e782
Mxx <496907723@qq.com>
2014-12-16 17:30:19 +0800

    代码复用

--- view/v_footer.php
+++ view/v_footer.php
@@ -2,7 +2,5 @@
 	require_once('../smarty/libs/Smarty.class.php');
 	header("Content-Type:text/html;charset=utf-8");
 	$index = new smarty();
-	require_once('view/v_header.php');
-	$index->display("template/main.tpl");
-	require_once('view/v_footer.php');
+	$index->display("template/footer.tpl");
 ?>
