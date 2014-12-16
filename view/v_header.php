4e327a6120fddcb4f08c35d1d973b09dc7d86c50
Huian Tang <447409030@qq.com>
2014-12-16 18:12:20 +0800

    ..

--- view/v_header.php
+++ view/v_header.php
@@ -2,12 +2,14 @@
 	require_once('../smarty/libs/Smarty.class.php');
 	header("Content-Type:text/html;charset=utf-8");
 	$index = new smarty();
-	if(isset($user_id)){
+
+	if(isset($_SESSION['user_id'])){
 		$index->assign("sign",'1');
 	}
 	else{
 		$index->assign("sign",'0');
 	}
+
 	GLOBAL $hotitem_main;
 	$index->assign("hotitem_main",$hotitem_main);
 	$index->display("template/header.tpl");	
