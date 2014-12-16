6261986c8f044e4ba21715e3ec55b378d289e55b
thierryNeo <mark041@qq.com>
2014-12-16 15:23:23 +0800

    更新了hotitem的内容

--- model/m_item.php
+++ model/m_item.php
@@ -13,11 +13,30 @@
 	  		$item_title		=	$_POST['item_title'];
 	  		$item_detail	=	$_POST['item_detail'];
 	  		$item_price		=	$_POST['item_price'];
-			$sql = "INSERT INTO shtp_item (item_img,user_id,item_type,item_title,item_detail,item_price) VALUES ('item_img',$user_id','$item_type','$item_title','$item_detail','$item_price')";
+			$sql = "INSERT INTO shtp_item (item_img,user_id,item_type,item_title,item_detail,item_price) VALUES ('item_img','$user_id','$item_type','$item_title','$item_detail','$item_price')";
 			$result_aitem = mysqli_query($con,$sql)
 			or die('添加商品失败！');
 			echo "成功添加商品！";
 			mysqli_close($con);	
+		}
+
+		public function hotitem(){ 
+			require_once('include/item.class.php');
+			require_once('include/connectvars.php');
+			//获取数据库连接变量
+			$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME)
+			or die('连接数据库失败！');
+	  		$sql = "SELECT item_img,item_title,item_price FROM shtp_item ORDER BY item_viewtime DESC";
+	  		$data = mysqli_query($con,$sql);
+	  		$arr=array();
+	  		$sum=0;
+	  		while($row = mysqli_fetch_array($data,MYSQL_ASSOC)){ 
+	  			if($sum==15)
+	  			break;
+	  			$arr[]=$row;
+	  			$sum++;
+	  		}		
+	  		mysqli_close($con);
 		}
  
 		public function edititem(){
@@ -34,12 +53,12 @@
 	  		$item_price	=	$_POST['item_price'];
 
 	  		$eitem = ("SELECT item_id,
-	  									 item_img,
-	  									 item_type,
-	  									 item_title,
-	  									 item_detail,
-	  									 item_price 
-	  							  FROM shtp_item WHERE item_id='$item_id'");
+	  						  item_img,
+	  						  item_type,
+	  						  item_title,
+	  						  item_detail,
+	  						  item_price 
+	  						FROM shtp_item WHERE item_id='$item_id'");
 	  		$result_eitem = mysqli_query($con,$eitem);
 
 	  		$uitem=("UPDATE shtp_item
