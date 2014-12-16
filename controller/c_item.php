d20cdf02fadba7ea3593eaa706bda48b9b6c1efb
thierryNeo <mark041@qq.com>
2014-12-16 15:51:59 +0800

    更新item,c类的view命名

--- controller/c_item.php
+++ controller/c_item.php
@@ -7,29 +7,29 @@
 					require_once('model/m_item.php');
 					$this->model = new m_item();
 					$this->model->additem();
-					require_once('view/v_viewitem.php');
+					require_once('view/v_view_item.php');
 					break;
 
 				case 'viewitem':
-					require_once('view/v_viewitem.php');
+					require_once('view/v_view_item.php');
 					break;
 
 				case 'hotitem':
 					require_once('model/m_item.php');
 					$this->model = new m_item();
 					$this->model->hotitem();
-					require_once('view/v_viewitem.php');
+					require_once('view/v_view_item.php');
 					break;
 					
 				case 'edititem':
 					require_once('model/m_item.php');
 					$this->model = new m_item();
 					$this->model->edititem();
-					require_once('view/v_viewitem.php');
+					require_once('view/v_view_item.php');
 					break;
 
 				case 'removeitem':
-					require_once('view/v_viewitem.php');
+					require_once('view/v_view_item.php');
 					break;
 			}
 		}
