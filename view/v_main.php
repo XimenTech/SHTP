5dce3ec13daeb536b172e4ab086c4ff45cbc6c4a
Mxx <496907723@qq.com>
2014-12-16 15:26:17 +0800

    头部大改
    还有热门商品数据库对接

--- view/v_main.php
+++ view/v_main.php
@@ -2,7 +2,11 @@
 	require_once('../smarty/libs/Smarty.class.php');
 	header("Content-Type:text/html;charset=utf-8");
 	$index = new smarty();
-	$index->display("template/header.tpl");
+	if(isset($user_id))
+		$index->assign("sign",'1');
+	else
+		$index->assign("sign",'0');
+	$index->display("template/header.tpl");	
 	$index->display("template/main.tpl");
 	$index->display("template/footer.tpl");
 ?>
